2b68e446b26c9e654dfc6ad067ff4ba41a034a5d
Edwin77-Axaz <eduin.abello7@gmail.com>
2024-02-10 20:44:01 -0500

    Three Commit

--- database.js
+++ database.js
@@ -9,6 +9,7 @@ const pool = mysql
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
         database: process.env.MYSQL_DATABASE,   
+        port    : process.env.MYSQL_PORT,
     })
     .promise();
 export async function getTodoByID(id){
